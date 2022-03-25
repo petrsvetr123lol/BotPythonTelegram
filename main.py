2dc07ad30e7ea63b87c575429deeafcfeb5d49b7
petrfilip <petafilip@email.cz>
2022-03-25 11:06:35 +0100

    trying to find option tag

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 from bs4 import BeautifulSoup
-import requests
+with open("index2.html", "r") as f:
+    doc = BeautifulSoup(f, "html.parser")
 
-url = "https://www.newegg.ca/gigabyte-geforce-rtx-3060-gv-n3060eagle-oc-12gd/p/N82E16814932434?Item=9SIAPBWFVW0978"
-result = requests.get(url)
-doc = BeautifulSoup(result.text, "html.parser")
-
-prices = doc.find_all(text="$")
-parent = prices[0].parent
-strong = parent.find("strong")
-print(strong.string)
+tag = doc.find("option")
+print(tag)
