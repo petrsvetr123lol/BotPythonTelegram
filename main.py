aa0a58cbd51e9c9d9ccd4d80bf272ee6771868fb
petrfilip <petafilip@email.cz>
2022-03-28 14:39:18 +0200

    working

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-from bs4 import BeautifulSoup
-with open("index2.html", "r") as f:
-    doc = BeautifulSoup(f, "html.parser")
+from bs4 import BeautifulSoup, SoupStrainer
+import requests
+import urllib.request
+import time
 
-tag = doc.find("option")
-print(tag)
+while True:
+    url = "https://github.com/petrsvetr123lol/Filip-Test-Vektory/commits"
+
+    result = requests.get(url)
+    doc = BeautifulSoup(result.text, "html.parser")
+
+    commit = doc.find(class_="Link--primary text-bold js-navigation-open markdown-title", href=True)
+    print(commit)
+
+    requests.post(
+        'https://api.telegram.org/bot5112690824:AAEwjrjQAxJtaWwyFDUa10pzltRBOAOqwS4/sendMessage?chat_id'
+        '=-604401103', data={
+            'text': commit
+        }).json()
