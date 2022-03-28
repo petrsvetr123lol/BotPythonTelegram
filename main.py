b2056fa107772ff8852b37d3c1dd8bdb524b1478
petrfilip <petafilip@email.cz>
2022-03-28 15:36:57 +0200

    repaired: 2 same commits

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
-from bs4 import BeautifulSoup, SoupStrainer
+from bs4 import BeautifulSoup
 import requests
-import urllib.request
 import time
 
+commit1 = "zero"
+commit2 = ""
+
 while True:
+
     url = "https://github.com/petrsvetr123lol/Filip-Test-Vektory/commits"
 
     result = requests.get(url)
     doc = BeautifulSoup(result.text, "html.parser")
 
-    commit = doc.find(class_="Link--primary text-bold js-navigation-open markdown-title", href=True)
-    print(commit)
+    commit1 = doc.find(class_="Link--primary text-bold js-navigation-open markdown-title", href=True)
+    print(commit1)
+    print(commit2)
 
-    requests.post(
-        'https://api.telegram.org/bot5112690824:AAEwjrjQAxJtaWwyFDUa10pzltRBOAOqwS4/sendMessage?chat_id'
-        '=-604401103', data={
-            'text': commit
-        }).json()
+    if commit1 != commit2:
+        requests.post(
+            'https://api.telegram.org/bot5112690824:AAEwjrjQAxJtaWwyFDUa10pzltRBOAOqwS4/sendMessage?chat_id'
+            '=-604401103', data={
+                'text': commit1
+            }).json()
+        commit2 = commit1
+    else:
+        requests.post(
+            'https://api.telegram.org/bot5112690824:AAEwjrjQAxJtaWwyFDUa10pzltRBOAOqwS4/sendMessage?chat_id'
+            '=-604401103', data={
+                'text': 'All commits were sent'
+            }).json()
+    time.sleep(1800)
