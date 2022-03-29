45e69d98886ceae3846d2587e59ee6fab0a32b04
petrfilip <petafilip@email.cz>
2022-03-29 11:02:36 +0200

    repaired: sending latest commit

--- main.py
+++ main.py
@@ -12,24 +12,21 @@ while True:
     result = requests.get(url)
     doc = BeautifulSoup(result.text, "html.parser")
 
-    commit1 = doc.find(class_="Link--primary text-bold js-navigation-open markdown-title", href=True)
+    commit1 = doc.find_all("a", "href", class_="Link--primary text-bold js-navigation-open markdown-title", href=True)
 
-    print(commit1)
+    links = []
+    for link in doc.find_all('a', class_="Link--primary text-bold js-navigation-open markdown-title", href=True):
+        links.append(link['href'])
+        send = links[0]
+        github_url = "https://github.com" + send
+    print(github_url)
+
     if commit1 != commit2:
+
         requests.post(
             'https://api.telegram.org/bot5112690824:AAEwjrjQAxJtaWwyFDUa10pzltRBOAOqwS4/sendMessage?chat_id'
             '=-604401103', data={
-                'text': 'Commited in'
-            }).json()
-        requests.post(
-            'https://api.telegram.org/bot5112690824:AAEwjrjQAxJtaWwyFDUa10pzltRBOAOqwS4/sendMessage?chat_id'
-            '=-604401103', data={
-                'text': url,
-            }).json()
-        requests.post(
-            'https://api.telegram.org/bot5112690824:AAEwjrjQAxJtaWwyFDUa10pzltRBOAOqwS4/sendMessage?chat_id'
-            '=-604401103', data={
-                'text': commit1,
+                'text': github_url,
             }).json()
         commit2 = commit1
     else:
@@ -38,4 +35,4 @@ while True:
             '=-604401103', data={
                 'text': 'All commits were sent'
             }).json()
-    time.sleep(1800)
+    time.sleep(20)
