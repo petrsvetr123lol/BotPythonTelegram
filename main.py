ffe6ccbee1f00e1e27804fc345edb33f61e6ff11
petrfilip <petafilip@email.cz>
2022-03-29 11:07:16 +0200

    comments

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ commit2 = ""
 
 while True:
 
+    ###our github repository
     url = "https://github.com/petrsvetr123lol/Filip-Test-Vektory/commits"
 
+    ###beautiful soup stuff
     result = requests.get(url)
     doc = BeautifulSoup(result.text, "html.parser")
 
+    ###finding latest commit - web scrapping
     links = []
     for link in doc.find_all('a', class_="Link--primary text-bold js-navigation-open markdown-title", href=True):
         links.append(link['href'])
@@ -19,6 +22,7 @@ while True:
         github_url = "https://github.com" + commit1
     print(github_url)
 
+    ###if - commit was not sent...
     if commit1 != commit2:
 
         requests.post(
@@ -27,10 +31,12 @@ while True:
                 'text': github_url,
             }).json()
         commit2 = commit1
+    ###else - commit was sent
     else:
         requests.post(
             'https://api.telegram.org/bot5112690824:AAEwjrjQAxJtaWwyFDUa10pzltRBOAOqwS4/sendMessage?chat_id'
             '=-604401103', data={
                 'text': 'All commits were sent'
             }).json()
-    time.sleep(20)
+    ###refresh eveery x seconds - default = 1800s
+    time.sleep(1800)
